498aa91085620312977fba77b405a1e2da023ed8
afterallafk <shivamatiet2001@gmail.com>
2022-11-27 00:24:17 +0530

    Pixel Extended Bringup
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,17 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Evolution-X Stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit common PixelExtended Stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_FACE_UNLOCK := true
 
-# Evolution-X Stuff
-EVO_BUILD_TYPE := OFFICIAL
+# PixelExtended Stuff
+PEX_BUILD_TYPE := UNOFFICIAL
 
-PRODUCT_NAME := evolution_sweet
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
