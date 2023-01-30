49e0f2c5e686e93f3783be5b4cf202a96a52a6ce
afterallafk <shivamatiet2001@gmail.com>
2023-01-31 00:07:20 +0530

    sweet: Clone hardware/xiaomi through vendorsetup
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 # clone
 git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
 git clone -b courbet-13 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+rm -rf hardware/xiaomi
+git clone https://github.com/afterallafk/hardware_xiaomi.git -b blaze-13 hardware/xiaomi
