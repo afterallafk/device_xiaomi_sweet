07dda975e19fa6568731eb13c470c2b5feb52d73
VEN0M98 <bt98.bt7@gmail.com>
2023-08-12 18:52:22 +0530

    sweet: Changes for APEX
    - enable OVERRIDE_TARGET_FLATTEN_APEX

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,9 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 
+# Apex
+OVERRIDE_TARGET_FLATTEN_APEX := true
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := sweet,sweetin
 
