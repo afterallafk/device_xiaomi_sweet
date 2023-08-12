48aa8f7f6c10ae3c8c4795a93949c035f949132c
afterallafk <shivamatiet2001@gmail.com>
2023-08-12 19:11:31 +0530

    sweet: Include vendorsetup.sh
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone --depth=1 https://github.com/afterallafk/kernel_xiaomi_sweet.git -b lineage-20 kernel/xiaomi/sweet
+git clone https://github.com/afterallafk/vendor_xiaomi_sweet.git -b 13 vendor/xiaomi/sweet
+rm -rf hardware/xiaomi
+git clone https://github.com/afterallafk/hardware_xiaomi.git -b blaze-13 hardware/xiaomi
+git clone https://gitlab.com/afterallafk/vendor_xiaomi_sweet-miuicamera.git -b 13 vendor/xiaomi/sweet-miuicamera
+git clone https://github.com/afterallafk/vendor_xiaomi_sweet-misound.git -b lineage-20 vendor/xiaomi/sweet-misound
