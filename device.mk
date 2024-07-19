bd6d1bd661eb24dde0caa57a9515e8199322b4c1
afterallafk <shivamatiet2001@gmail.com>
2024-07-19 14:09:50 +0530

    Bringup EuclidOS
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- device.mk
+++ device.mk
@@ -65,7 +65,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-blaze
+    $(LOCAL_PATH)/overlay-euclid
 
 # Overlay-RRO
 PRODUCT_PACKAGES += \
