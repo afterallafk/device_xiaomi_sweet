dc722ba7bb55240ef0dc320aabb0f94b1e6288e9
afterallafk <shivamatiet2001@gmail.com>
2024-12-11 11:57:26 +0530

    Project Flare Bringup
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/flare_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    flare_sweet-ap4a-user \
+    flare_sweet-ap4a-userdebug \
+    flare_sweet-ap4a-eng
